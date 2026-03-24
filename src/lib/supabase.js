9a30e32b2fdcddcef1396fed590492d390d07f3a
RDMNCHN <121752802+RDMNCHN@users.noreply.github.com>
2026-03-24 23:19:00 +0100

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://ghwohwblgtryienupzkh.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdod29od2JsZ3RyeWllbnVwemtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxNzMxMTAsImV4cCI6MjA4OTc0OTExMH0.yioE8W9L0WGIiYTZ7F1c62bRj3yzJQO8obLD66APmlI'
-)
